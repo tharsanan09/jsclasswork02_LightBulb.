17097afd568d49a47ec18d3f25a2f7abc4b71cfe
tharsanan09 <tharshanantharsan@gmail.com>
2025-03-13 15:25:45 +0530

    4th commit

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
     const offbutton = document.getElementById("offbutton");
 
     function turnon(){
-        bulbimage.src = "/home/uki-jaffna/Documents/tharsh/js2 question2/image/light.pnggit ";
+        bulbimage.src = "./image/light.png";
     }
 
         function turnoff(){
-            bulbimage.src="/home/uki-jaffna/Documents/tharsh/js2 question2/image/images.jpeg";
+            bulbimage.src="./image/images.jpeg";
 
 
         }
