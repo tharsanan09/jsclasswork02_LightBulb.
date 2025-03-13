9f513bed48680eac5ae5ad2fe88f5b9e9ec1a7cc
tharsanan09 <tharshanantharsan@gmail.com>
2025-03-13 14:53:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+   
+
+    // project2
+    const bulbimage= document.getElementById("bulbimage");
+    const onbutton = document.getElementById("onbutton");
+    const offbutton = document.getElementById("offbutton");
+
+    function turnon(){
+        bulbimage.src = "/home/uki-jaffna/Documents/tharsh/js2/image/light.png";
+    }
+
+        function turnoff(){
+            bulbimage.src="/home/uki-jaffna/Documents/tharsh/js2/image/images.jpeg";
+
+
+        }
+
+        // project3
+
+
+  
